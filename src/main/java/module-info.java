dd946dbec800c635594c50e4590df4eaa39a2172
Iamkweku <pklawson@st.ug.edu.gh>
2023-07-20 14:35:06 +0000

    final work done

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,10 @@
-module com.example.pharmacy_management_system {
-  
-    // Requires the JavaFX controls module for using JavaFX UI components.
+module com.example.pharmacymanagementsystem {
     requires javafx.controls;
-   
-    // Requires the JavaFX FXML module for using FXML files to define the UI layout.
     requires javafx.fxml;
 
-
-    // Opens the package com.example.pharmacy_management_system to the JavaFX FXML module.
-    // This allows JavaFX to access and interact with classes in this package when loading FXML files.
-    opens com.example.pharmacy_management_system to javafx.fxml;
-    
-    // Exports the package com.example.pharmacy_management_system.
-    // This makes the classes in this package accessible to other modules that depend on this module.
-    exports com.example.pharmacy_management_system;
+    requires java.sql;
+            
+                            
+    opens com.example.pharmacymanagementsystem to javafx.fxml;
+    exports com.example.pharmacymanagementsystem;
 }
